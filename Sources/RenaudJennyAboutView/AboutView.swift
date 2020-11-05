7c91b9715f3f52e73eeb583ebc5be7e095e83c81
Renaud Jenny <renaud.jenny@gmail.com>
2020-11-05 21:12:08 +0000

    feat(Modal): handle displaying AboutView in a modal
    * Like it's done in MixTeam

--- Sources/RenaudJennyAboutView/AboutView.swift
+++ Sources/RenaudJennyAboutView/AboutView.swift
@@ -1,27 +1,57 @@
 import SwiftUI
 
-public struct AboutView<Logo: View>: View {
+public struct AboutView<Logo: View, Background: View>: View {
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+    @Environment(\.colorScheme) var colorScheme
+    @Environment(\.presentationMode) var presentationMode
+
     let appId: String
     let logo: Logo
+    let isInModal: Bool
+    var background: Background
 
-    public init(appId: String, @ViewBuilder logo: () -> Logo) {
+    public init(
+        appId: String,
+        isInModal: Bool = false,
+        @ViewBuilder logo: () -> Logo,
+        @ViewBuilder background: () -> Background
+    ) {
         self.appId = appId
+        self.isInModal = isInModal
         self.logo = logo()
+        self.background = background()
     }
 
     public var body: some View {
         ScrollView(.vertical) {
             HStack { Spacer() }
-            logo.padding()
-            VStack(spacing: 32) {
-                developmentCredit
-                openSourceCredit
-                iconsAndIllustrationsCredit
-                Text("Thank you for your support!")
-                    .multilineTextAlignment(.center)
-                    .font(.headline)
-                rateThisApp
-            }.padding()
+            if isInModal {
+                if verticalSizeClass == .regular {
+                    closeCapsule
+                } else {
+                    HStack {
+                        Spacer()
+                        Button(action: close) {
+                            Text("Done")
+                        }.padding()
+                    }
+                }
+            }
+            VStack {
+                logo.padding()
+                VStack(spacing: 32) {
+                    developmentCredit
+                    openSourceCredit
+                    iconsAndIllustrationsCredit
+                    Text("Thank you for your support!")
+                        .multilineTextAlignment(.center)
+                        .font(.headline)
+                    rateThisApp
+                }
+                .padding()
+
+            }
+            .background(background)
         }
         .navigationBarTitle("About")
     }
@@ -60,6 +90,35 @@ public struct AboutView<Logo: View>: View {
                 .multilineTextAlignment(.center)
 
         }
+    }
+
+    private func close() {
+        presentationMode.wrappedValue.dismiss()
+    }
+
+    private var closeCapsule: some View {
+        Capsule(style: .circular)
+            .fill(
+                colorScheme == .light
+                    ? Color.black.opacity(1/4)
+                    : Color.white.opacity(1/4)
+            )
+            .frame(width: 50, height: 5)
+    }
+}
+
+public extension AboutView where Background == EmptyView {
+    init(
+        appId: String,
+        isInModal: Bool = false,
+        @ViewBuilder logo: () -> Logo
+    ) {
+        self.init(
+            appId: appId,
+            isInModal: isInModal,
+            logo: logo,
+            background: { EmptyView() }
+        )
     }
 }
 
@@ -125,6 +184,76 @@ struct AboutView_Previews: PreviewProvider {
                     .frame(width: 120, height: 120)
             }
         }
+    }
+}
+
+struct AboutViewInModal_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutView(appId: "id123456", isInModal: true) {
+            Image(systemName: "questionmark.circle")
+                .resizable()
+                .frame(width: 120, height: 120)
+        }
+        .environment(\.verticalSizeClass, .regular)
+    }
+}
+
+struct AboutViewInModalDarkMode_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutView(appId: "id123456", isInModal: true) {
+            Image(systemName: "questionmark.circle")
+                .resizable()
+                .frame(width: 120, height: 120)
+        }
+        .background(Color(UIColor.systemBackground))
+        .edgesIgnoringSafeArea(.all)
+        .colorScheme(.dark)
+        .environment(\.verticalSizeClass, .regular)
+    }
+}
+
+struct AboutViewInModalLandscape_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            AboutView(appId: "id123456", isInModal: true) {
+                Image(systemName: "questionmark.circle")
+                    .resizable()
+                    .frame(width: 120, height: 120)
+            }
+            .previewLayout(.fixed(width: 568, height: 320))
+            .environment(\.verticalSizeClass, .compact)
+        }
+    }
+}
+
+struct AboutViewInModalWithBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutView(appId: "id123456", isInModal: true) {
+            Image(systemName: "questionmark.circle")
+                .resizable()
+                .frame(width: 120, height: 120)
+        } background: {
+            Color.red.opacity(1/10)
+                .cornerRadius(20)
+        }
+        .edgesIgnoringSafeArea(.all)
+        .environment(\.verticalSizeClass, .regular)
+    }
+}
+
+struct AboutViewInModalWithBackgroundLandscape_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutView(appId: "id123456", isInModal: true) {
+            Image(systemName: "questionmark.circle")
+                .resizable()
+                .frame(width: 120, height: 120)
+        } background: {
+            Color.red.opacity(1/10)
+                .cornerRadius(20)
+        }
+        .edgesIgnoringSafeArea(.all)
+        .previewLayout(.fixed(width: 568, height: 320))
+        .environment(\.verticalSizeClass, .compact)
     }
 }
 #endif
--- Tests/RenaudJennyAboutViewTests/AboutViewTests.swift
+++ Tests/RenaudJennyAboutViewTests/AboutViewTests.swift
@@ -8,7 +8,37 @@ final class RenaudJennyAboutViewTests: XCTestCase {
         assertSnapshot(matching: previews, as: .image(layout: .device(config: .iPhoneSe)))
     }
 
+    func testAboutViewInModal() {
+        let previews = AboutViewInModal_Previews.previews
+        assertSnapshot(matching: previews, as: .image(layout: .device(config: .iPhoneSe)))
+    }
+
+    func testAboutViewInModalDarkMode() {
+        let previews = AboutViewInModalDarkMode_Previews.previews
+        assertSnapshot(matching: previews, as: .image(layout: .device(config: .iPhoneSe)))
+    }
+
+    func testAboutViewInModalLandscape() {
+        let previews = AboutViewInModalLandscape_Previews.previews
+        assertSnapshot(matching: previews, as: .image(layout: .device(config: .iPhoneSe(.landscape))))
+    }
+
+    func testAboutViewInModalWithBackground() {
+        let previews = AboutViewInModalWithBackground_Previews.previews
+        assertSnapshot(matching: previews, as: .image(layout: .device(config: .iPhoneSe)))
+    }
+
+    func testAboutViewInModalWithBackgroundLandscape() {
+        let previews = AboutViewInModalWithBackgroundLandscape_Previews.previews
+        assertSnapshot(matching: previews, as: .image(layout: .device(config: .iPhoneSe(.landscape))))
+    }
+
     static var allTests = [
         ("testTheAboutView", testTheAboutView),
+        ("testAboutViewInModal", testAboutViewInModal),
+        ("testAboutViewInModalDarkMode", testAboutViewInModalDarkMode),
+        ("testAboutViewInModalLandscape", testAboutViewInModalLandscape),
+        ("testAboutViewInModalWithBackground", testAboutViewInModalWithBackground),
+        ("testAboutViewInModalWithBackgroundLandscape", testAboutViewInModalWithBackgroundLandscape),
     ]
 }
